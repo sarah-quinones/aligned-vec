63a1c66374246b154450dff7f6c42c20fe8d87d1
sarah <>
2022-07-24 11:23:36 +0200

    feat: impl avec and abox

---
+++ src/lib.rs
@@ -0,0 +1,556 @@
+use std::{
+    marker::PhantomData,
+    mem::{align_of, size_of, ManuallyDrop},
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+};
+
+use raw::ARawVec;
+
+mod raw;
+
+/// Aligned vector. See [`Vec`] for more info.
+pub struct AVec<T> {
+    buf: ARawVec<T>,
+    len: usize,
+}
+
+/// Aligned box. See [`Box`] for more info.
+pub struct ABox<T: ?Sized> {
+    ptr: NonNull<T>,
+    align: usize,
+    _marker: PhantomData<T>,
+}
+
+impl<T: ?Sized> Deref for ABox<T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr.as_ptr() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for ABox<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.ptr.as_ptr() }
+    }
+}
+
+struct AllocDrop {
+    ptr: *mut u8,
+    size_bytes: usize,
+    align: usize,
+}
+impl Drop for AllocDrop {
+    #[inline]
+    fn drop(&mut self) {
+        if self.size_bytes > 0 {
+            unsafe {
+                std::alloc::dealloc(
+                    self.ptr,
+                    std::alloc::Layout::from_size_align_unchecked(self.size_bytes, self.align),
+                )
+            }
+        }
+    }
+}
+
+impl<T: ?Sized> Drop for ABox<T> {
+    #[inline]
+    fn drop(&mut self) {
+        let size_bytes = core::mem::size_of_val(self.deref_mut());
+        let ptr = self.deref_mut() as *mut T;
+        let _alloc_drop = AllocDrop {
+            ptr: ptr as *mut u8,
+            size_bytes,
+            align: self.align,
+        };
+        unsafe { ptr.drop_in_place() };
+    }
+}
+
+impl<T> Deref for AVec<T> {
+    type Target = [T];
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_slice()
+    }
+}
+impl<T> DerefMut for AVec<T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut_slice()
+    }
+}
+
+impl<T> ABox<T> {
+    /// Creates a new [`ABox<T>`] containing `value` at an address aligned to `align` bytes.
+    #[inline]
+    pub fn new(value: T, align: usize) -> Self {
+        let ptr = if size_of::<T>() == 0 {
+            NonNull::<T>::dangling().as_ptr()
+        } else {
+            unsafe {
+                raw::with_capacity_unchecked(
+                    1,
+                    fix_alignment(align, align_of::<T>()),
+                    size_of::<T>(),
+                ) as *mut T
+            }
+        };
+        unsafe { ptr.write(value) };
+        unsafe { Self::from_raw_parts(ptr, align) }
+    }
+}
+
+impl<T: ?Sized> ABox<T> {
+    /// Creates a new [`ABox<T>`] from its raw parts.
+    ///
+    /// # Safety
+    ///
+    /// The arguments to this function must be acquired from a previous call to
+    /// [`Self::into_raw_parts`].
+    #[inline]
+    pub unsafe fn from_raw_parts(ptr: *mut T, align: usize) -> Self {
+        Self {
+            ptr: NonNull::<T>::new_unchecked(ptr),
+            align,
+            _marker: PhantomData,
+        }
+    }
+
+    /// Decomposes a [`ABox<T>`] into its raw parts: `(ptr, alignment)`.
+    #[inline]
+    pub fn into_raw_parts(self) -> (*mut T, usize) {
+        let this = ManuallyDrop::new(self);
+        (this.ptr.as_ptr(), this.align)
+    }
+}
+
+impl<T> Drop for AVec<T> {
+    #[inline]
+    fn drop(&mut self) {
+        // SAFETY: dropping initialized elements
+        unsafe { (self.as_mut_slice() as *mut [T]).drop_in_place() }
+    }
+}
+
+#[inline]
+fn fix_alignment(align: usize, base_align: usize) -> usize {
+    match align.checked_next_power_of_two() {
+        Some(val) => val,
+        None => 0,
+    }
+    .max(base_align)
+}
+
+impl<T> AVec<T> {
+    /// Returns a new [`AVec<T>`] with the provided alignment.
+    #[inline]
+    #[must_use]
+    pub fn new(align: usize) -> Self {
+        unsafe {
+            Self {
+                buf: ARawVec::new_unchecked(fix_alignment(align, align_of::<T>())),
+                len: 0,
+            }
+        }
+    }
+
+    /// Creates a new empty vector with enough capacity for at least `capacity` elements to
+    /// be inserted in the vector. If `capacity` is 0, the vector will not allocate.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the capacity exceeds `isize::MAX` bytes.
+    #[inline]
+    #[must_use]
+    pub fn with_capacity(capacity: usize, align: usize) -> Self {
+        unsafe {
+            Self {
+                buf: ARawVec::with_capacity_unchecked(
+                    capacity,
+                    fix_alignment(align, align_of::<T>()),
+                ),
+                len: 0,
+            }
+        }
+    }
+
+    /// Returns a new [`AVec<T>`] from its raw parts.
+    ///
+    /// # Safety
+    ///
+    /// The arguments to this function must be acquired from a previous call to
+    /// [`Self::into_raw_parts`].
+    #[inline]
+    #[must_use]
+    pub unsafe fn from_raw_parts(ptr: *mut T, len: usize, capacity: usize, align: usize) -> Self {
+        Self {
+            buf: ARawVec::from_raw_parts(ptr, capacity, align),
+            len,
+        }
+    }
+
+    /// Decomposes an [`AVec<T>`] into its raw parts: `(ptr, length, capacity, alignment)`.
+    #[inline]
+    pub fn into_raw_parts(self) -> (*mut T, usize, usize, usize) {
+        let mut this = ManuallyDrop::new(self);
+        let len = this.len();
+        let cap = this.capacity();
+        let align = this.align();
+        let ptr = this.as_mut_ptr();
+        (ptr, len, cap, align)
+    }
+
+    /// Returns the length of the vector.
+    #[inline]
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Returns `true` if the vector's length is equal to `0`, and false otherwise.
+    #[inline]
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Returns the number of elements the vector can hold without needing to reallocate.
+    #[inline]
+    #[must_use]
+    pub fn capacity(&self) -> usize {
+        self.buf.capacity()
+    }
+
+    /// Reserves enough capacity for at least `additional` more elements to be inserted in the
+    /// vector. After this call to `reserve`, capacity will be greater than or equal to `self.len()
+    /// + additional`. Does nothing if the capacity is already sufficient.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new capacity exceeds `isize::MAX` bytes.
+    #[inline]
+    pub fn reserve(&mut self, additional: usize) {
+        if additional > self.capacity().wrapping_sub(self.len) {
+            unsafe { self.buf.grow_amortized(self.len, additional) };
+        }
+    }
+
+    /// Reserves enough capacity for exactly `additional` more elements to be inserted in the
+    /// vector. After this call to `reserve`, capacity will be greater than or equal to `self.len()
+    /// + additional`. Does nothing if the capacity is already sufficient.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new capacity exceeds `isize::MAX` bytes.
+    #[inline]
+    pub fn reserve_exact(&mut self, additional: usize) {
+        if additional > self.capacity().wrapping_sub(self.len) {
+            unsafe { self.buf.grow_exact(self.len, additional) };
+        }
+    }
+
+    /// Returns the alignment of the vector.
+    #[inline]
+    #[must_use]
+    pub fn align(&self) -> usize {
+        self.buf.align()
+    }
+
+    /// Returns a pointer to the objects held by the vector.
+    #[inline]
+    #[must_use]
+    pub fn as_ptr(&self) -> *const T {
+        self.buf.as_ptr()
+    }
+
+    /// Returns a mutable pointer to the objects held by the vector.
+    #[inline]
+    #[must_use]
+    pub fn as_mut_ptr(&mut self) -> *mut T {
+        self.buf.as_mut_ptr()
+    }
+
+    /// Returns a reference to a slice over the objects held by the vector.
+    #[inline]
+    #[must_use]
+    pub fn as_slice(&self) -> &[T] {
+        let len = self.len();
+        let ptr = self.as_ptr();
+
+        // ptr points to `len` initialized elements and is properly aligned since
+        // self.align is at least `align_of::<T>()`
+        unsafe { std::slice::from_raw_parts(ptr, len) }
+    }
+
+    /// Returns a mutable reference to a slice over the objects held by the vector.
+    #[inline]
+    #[must_use]
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        let len = self.len();
+        let ptr = self.as_mut_ptr();
+
+        // ptr points to `len` initialized elements and is properly aligned since
+        // self.align is at least `align_of::<T>()`
+        unsafe { std::slice::from_raw_parts_mut(ptr, len) }
+    }
+
+    /// Push the given value to the end of the vector, reallocating if needed.
+    #[inline]
+    pub fn push(&mut self, value: T) {
+        if self.len == self.capacity() {
+            unsafe { self.buf.grow_amortized(self.len, 1) };
+        }
+
+        // SAFETY: self.capacity is greater than self.len so the write is valid
+        unsafe {
+            let past_the_end = self.as_mut_ptr().add(self.len);
+            past_the_end.write(value);
+            self.len += 1;
+        }
+    }
+
+    /// Remove the last value from the vector if it exists, otherwise returns `None`.
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            // SAFETY: the len was greater than one so we had one valid element at the last address
+            Some(unsafe { self.as_mut_ptr().add(self.len()).read() })
+        }
+    }
+
+    /// Shrinks the capacity of the vector with a lower bound.  
+    /// The capacity will remain at least as large as both the length and the supplied value.  
+    /// If the current capacity is less than the lower limit, this is a no-op.
+    #[inline]
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        let min_capacity = min_capacity.max(self.len());
+        if self.capacity() > min_capacity {
+            unsafe { self.buf.shrink_to(min_capacity) };
+        }
+    }
+
+    /// Shrinks the capacity of the vector as much as possible without dropping any elements.  
+    #[inline]
+    pub fn shrink_to_fit(&mut self) {
+        if self.capacity() > self.len {
+            unsafe { self.buf.shrink_to(self.len) };
+        }
+    }
+
+    /// Drops the last elements of the vector until its length is equal to `len`.  
+    /// If `len` is greater than or equal to `self.len()`, this is a no-op.
+    #[inline]
+    pub fn truncate(&mut self, len: usize) {
+        if len < self.len {
+            let old_len = self.len;
+            self.len = len;
+            unsafe {
+                let ptr = self.as_mut_ptr();
+                std::ptr::slice_from_raw_parts_mut(ptr.add(len), old_len - len).drop_in_place()
+            }
+        }
+    }
+
+    /// Drops the all the elements of the vector, setting its length to `0`.
+    #[inline]
+    pub fn clear(&mut self) {
+        let old_len = self.len;
+        self.len = 0;
+        unsafe {
+            let ptr = self.as_mut_ptr();
+            std::ptr::slice_from_raw_parts_mut(ptr, old_len).drop_in_place()
+        }
+    }
+
+    /// Converts the vector into [`ABox<T>`].  
+    /// This will drop any excess capacity.
+    #[inline]
+    pub fn into_boxed_slice(self) -> ABox<[T]> {
+        let mut this = self;
+        this.shrink_to_fit();
+        let (ptr, len, _, align) = this.into_raw_parts();
+        unsafe { ABox::<[T]>::from_raw_parts(std::ptr::slice_from_raw_parts_mut(ptr, len), align) }
+    }
+
+    /// Collects an iterator into an [`AVec<T>`] with the provided alignment.
+    #[inline]
+    pub fn from_iter<I: IntoIterator<Item = T>>(iter: I, align: usize) -> Self {
+        Self::from_iter_impl(iter.into_iter(), align)
+    }
+
+    fn from_iter_impl<I: Iterator<Item = T>>(mut iter: I, align: usize) -> Self {
+        let lower_bound = iter.size_hint().0;
+        let mut this = Self::with_capacity(lower_bound, align);
+
+        {
+            let len = &mut this.len;
+            let ptr = this.buf.ptr.as_ptr();
+
+            let first_chunk = (&mut iter).take(lower_bound);
+            for (i, item) in first_chunk.enumerate() {
+                unsafe { ptr.add(i).write(item) };
+                *len += 1;
+            }
+        }
+
+        for item in iter {
+            this.push(item);
+        }
+
+        this
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter::repeat;
+
+    use super::*;
+
+    #[test]
+    fn new() {
+        let v = AVec::<i32>::new(15);
+        assert_eq!(v.len(), 0);
+        assert_eq!(v.capacity(), 0);
+        assert_eq!(v.align(), 16);
+        let v = AVec::<()>::new(15);
+        assert_eq!(v.len(), 0);
+        assert_eq!(v.capacity(), usize::MAX);
+        assert_eq!(v.align(), 16);
+    }
+
+    #[test]
+    fn collect() {
+        let v = AVec::from_iter(0..4, 64);
+        assert_eq!(&*v, &[0, 1, 2, 3]);
+        let v = AVec::from_iter(repeat(()).take(4), 64);
+        assert_eq!(&*v, &[(), (), (), ()]);
+    }
+
+    #[test]
+    fn push() {
+        let mut v = AVec::<i32>::new(16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        assert_eq!(&*v, &[0, 1, 2, 3]);
+
+        let mut v = AVec::from_iter(0..4, 64);
+        v.push(4);
+        v.push(5);
+        v.push(6);
+        v.push(7);
+        assert_eq!(&*v, &[0, 1, 2, 3, 4, 5, 6, 7]);
+
+        let mut v = AVec::from_iter(repeat(()).take(4), 64);
+        v.push(());
+        v.push(());
+        v.push(());
+        v.push(());
+        assert_eq!(&*v, &[(), (), (), (), (), (), (), ()]);
+    }
+
+    #[test]
+    fn pop() {
+        let mut v = AVec::<i32>::new(16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        assert_eq!(v.pop(), Some(3));
+        assert_eq!(v.pop(), Some(2));
+        assert_eq!(v.pop(), Some(1));
+        assert_eq!(v.pop(), Some(0));
+        assert_eq!(v.pop(), None);
+        assert_eq!(v.pop(), None);
+        assert_eq!(&*v, &[]);
+        assert!(v.is_empty());
+
+        let mut v = AVec::<()>::new(16);
+        v.push(());
+        v.push(());
+        v.push(());
+        v.push(());
+        assert_eq!(v.pop(), Some(()));
+        assert_eq!(v.pop(), Some(()));
+        assert_eq!(v.pop(), Some(()));
+        assert_eq!(v.pop(), Some(()));
+        assert_eq!(v.pop(), None);
+        assert_eq!(v.pop(), None);
+        assert_eq!(&*v, &[]);
+        assert!(v.is_empty());
+    }
+
+    #[test]
+    fn shrink() {
+        let mut v = AVec::<i32>::with_capacity(10, 16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+
+        assert_eq!(v.capacity(), 10);
+        v.shrink_to_fit();
+        assert_eq!(v.len(), 3);
+        assert_eq!(v.capacity(), 3);
+
+        let mut v = AVec::<i32>::with_capacity(10, 16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+
+        assert_eq!(v.capacity(), 10);
+        v.shrink_to(0);
+        assert_eq!(v.len(), 3);
+        assert_eq!(v.capacity(), 3);
+    }
+
+    #[test]
+    fn truncate() {
+        let mut v = AVec::<i32>::new(16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+
+        v.truncate(1);
+        assert_eq!(v.len(), 1);
+        assert_eq!(&*v, &[0]);
+
+        v.clear();
+        assert_eq!(v.len(), 0);
+        assert_eq!(&*v, &[]);
+
+        let mut v = AVec::<()>::new(16);
+        v.push(());
+        v.push(());
+        v.push(());
+
+        v.truncate(1);
+        assert_eq!(v.len(), 1);
+        assert_eq!(&*v, &[()]);
+
+        v.clear();
+        assert_eq!(v.len(), 0);
+        assert_eq!(&*v, &[]);
+    }
+
+    #[test]
+    fn into_boxed_slice() {
+        let mut v = AVec::<i32>::new(16);
+        v.push(0);
+        v.push(1);
+        v.push(2);
+
+        let boxed = v.into_boxed_slice();
+        assert_eq!(&*boxed, &[0, 1, 2]);
+    }
+}
---
+++ src/raw.rs
@@ -0,0 +1,249 @@
+use std::{
+    alloc::{alloc, dealloc, handle_alloc_error, realloc, Layout},
+    marker::PhantomData,
+    mem::size_of,
+    ptr::{null_mut, NonNull},
+};
+
+pub struct ARawVec<T> {
+    pub ptr: NonNull<T>,
+    pub capacity: usize,
+    pub align: usize,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Drop for ARawVec<T> {
+    #[inline]
+    fn drop(&mut self) {
+        // this can't overflow since we already have this much stored in a slice
+        let size_bytes = self.capacity * size_of::<T>();
+        if size_bytes > 0 {
+            // SAFETY: memory was allocated with std::alloc::alloc
+            unsafe {
+                dealloc(
+                    self.ptr.as_ptr() as *mut u8,
+                    Layout::from_size_align_unchecked(size_bytes, self.align),
+                )
+            }
+        }
+    }
+}
+
+pub fn capacity_overflow() -> ! {
+    panic!("capacity overflow")
+}
+
+impl<T> ARawVec<T> {
+    /// # Safety
+    ///
+    /// `align` must be a power of two.  
+    /// `align` must be greater than or equal to `std::mem::align_of::<T>()`.
+    #[inline]
+    pub unsafe fn new_unchecked(align: usize) -> Self {
+        let cap = if size_of::<T>() == 0 { usize::MAX } else { 0 };
+        Self::from_raw_parts(null_mut::<u8>().wrapping_add(align) as *mut T, cap, align)
+    }
+
+    /// # Safety
+    ///
+    /// `align` must be a power of two.  
+    /// `align` must be greater than or equal to `std::mem::align_of::<T>()`.
+    #[inline]
+    pub unsafe fn with_capacity_unchecked(capacity: usize, align: usize) -> Self {
+        if capacity == 0 || size_of::<T>() == 0 {
+            Self::new_unchecked(align)
+        } else {
+            Self {
+                ptr: NonNull::<T>::new_unchecked(with_capacity_unchecked(
+                    capacity,
+                    align,
+                    size_of::<T>(),
+                ) as *mut T),
+                capacity,
+                align,
+                _marker: PhantomData,
+            }
+        }
+    }
+
+    const MIN_NON_ZERO_CAP: usize = if size_of::<T>() == 1 {
+        8
+    } else if size_of::<T>() <= 1024 {
+        4
+    } else {
+        1
+    };
+
+    pub unsafe fn grow_amortized(&mut self, len: usize, additional: usize) {
+        debug_assert!(additional > 0);
+        if self.capacity == 0 {
+            *self =
+                Self::with_capacity_unchecked(additional.max(Self::MIN_NON_ZERO_CAP), self.align);
+            return;
+        }
+
+        if size_of::<T>() == 0 {
+            debug_assert_eq!(self.capacity, usize::MAX);
+            capacity_overflow();
+        }
+
+        let new_cap = match len.checked_add(additional) {
+            Some(cap) => cap,
+            None => capacity_overflow(),
+        };
+
+        // self.cap * 2 can't overflow because it's less than isize::MAX
+        let new_cap = new_cap.max(self.capacity * 2);
+        let new_cap = new_cap.max(Self::MIN_NON_ZERO_CAP);
+
+        let ptr = {
+            grow_unchecked(
+                self.as_mut_ptr() as *mut u8,
+                self.capacity,
+                new_cap,
+                self.align,
+                size_of::<T>(),
+            ) as *mut T
+        };
+
+        self.capacity = new_cap;
+        self.ptr = NonNull::<T>::new_unchecked(ptr);
+    }
+
+    pub unsafe fn grow_exact(&mut self, len: usize, additional: usize) {
+        debug_assert!(additional > 0);
+        if size_of::<T>() == 0 {
+            debug_assert_eq!(self.capacity, usize::MAX);
+            capacity_overflow();
+        }
+
+        if self.capacity == 0 {
+            *self = Self::with_capacity_unchecked(additional, self.align);
+            return;
+        }
+
+        let new_cap = match len.checked_add(additional) {
+            Some(cap) => cap,
+            None => capacity_overflow(),
+        };
+
+        let ptr = grow_unchecked(
+            self.as_mut_ptr() as *mut u8,
+            self.capacity,
+            new_cap,
+            self.align,
+            size_of::<T>(),
+        ) as *mut T;
+
+        self.capacity = new_cap;
+        self.ptr = NonNull::<T>::new_unchecked(ptr);
+    }
+
+    pub unsafe fn shrink_to(&mut self, len: usize) {
+        if size_of::<T>() == 0 {
+            return;
+        }
+
+        debug_assert!(len < self.capacity());
+        let size_of = size_of::<T>();
+        let old_capacity = self.capacity;
+        let align = self.align;
+        let old_ptr = self.ptr.as_ptr() as *mut u8;
+
+        // this cannot overflow or exceed isize::MAX bytes since len < cap and the same was true
+        // for cap
+        let new_size_bytes = len * size_of;
+        let old_size_bytes = old_capacity * size_of;
+        let old_layout = Layout::from_size_align_unchecked(old_size_bytes, align);
+
+        let ptr = realloc(old_ptr, old_layout, new_size_bytes);
+        let ptr = ptr as *mut T;
+        self.capacity = len;
+        self.ptr = NonNull::<T>::new_unchecked(ptr);
+    }
+
+    #[inline]
+    pub unsafe fn from_raw_parts(ptr: *mut T, capacity: usize, align: usize) -> Self {
+        Self {
+            ptr: NonNull::<T>::new_unchecked(ptr),
+            capacity,
+            align,
+            _marker: PhantomData,
+        }
+    }
+
+    /// Returns the capacity of the vector.
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    #[inline]
+    pub fn align(&self) -> usize {
+        self.align
+    }
+
+    #[inline]
+    pub fn as_ptr(&self) -> *const T {
+        self.ptr.as_ptr()
+    }
+
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut T {
+        self.ptr.as_ptr()
+    }
+}
+
+pub unsafe fn with_capacity_unchecked(capacity: usize, align: usize, size_of: usize) -> *mut u8 {
+    let size_bytes = match capacity.checked_mul(size_of) {
+        Some(size_bytes) => size_bytes,
+        None => capacity_overflow(),
+    };
+    debug_assert!(size_bytes > 0);
+    let will_overflow = size_bytes > usize::MAX - (align - 1);
+    if will_overflow || !is_valid_alloc(size_bytes) {
+        capacity_overflow();
+    }
+
+    let layout = Layout::from_size_align_unchecked(size_bytes, align);
+    let ptr = alloc(layout);
+    if ptr.is_null() {
+        handle_alloc_error(layout);
+    }
+    ptr
+}
+
+unsafe fn grow_unchecked(
+    old_ptr: *mut u8,
+    old_capacity: usize,
+    new_capacity: usize,
+    align: usize,
+    size_of: usize,
+) -> *mut u8 {
+    let new_size_bytes = match new_capacity.checked_mul(size_of) {
+        Some(size_bytes) => size_bytes,
+        None => capacity_overflow(),
+    };
+    let will_overflow = new_size_bytes > usize::MAX - (align - 1);
+    if will_overflow || !is_valid_alloc(new_size_bytes) {
+        capacity_overflow();
+    }
+
+    // can't overflow because we already allocated this much
+    let old_size_bytes = old_capacity * size_of;
+    let old_layout = Layout::from_size_align_unchecked(old_size_bytes, align);
+
+    let ptr = realloc(old_ptr, old_layout, new_size_bytes);
+
+    if ptr.is_null() {
+        let new_layout = Layout::from_size_align_unchecked(old_size_bytes, align);
+        handle_alloc_error(new_layout);
+    }
+
+    ptr
+}
+
+#[inline]
+fn is_valid_alloc(alloc_size: usize) -> bool {
+    !(usize::BITS < 64 && alloc_size > isize::MAX as usize)
+}
